ec9ff88400add48f3e6b862510d86a61e3966138
Aditya <lalwaniaditya4@gmail.com>
2026-05-27 18:04:14 +0000

    infra: manage IAM roles with terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,3 +12,24 @@ resource "google_project_service" "firestore" {
   project = var.project_id
   service = "firestore.googleapis.com"
 }
+
+resource "google_project_iam_member" "cloudbuild_builder" {
+  project = var.project_id
+  role    = "roles/cloudbuild.builds.builder"
+
+  member = "serviceAccount:357158911015-compute@developer.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "artifact_registry_writer" {
+  project = var.project_id
+  role    = "roles/artifactregistry.writer"
+
+  member = "serviceAccount:357158911015-compute@developer.gserviceaccount.com"
+}
+
+resource "google_project_iam_member" "firestore_user" {
+  project = var.project_id
+  role    = "roles/datastore.user"
+
+  member = "serviceAccount:357158911015-compute@developer.gserviceaccount.com"
+}
